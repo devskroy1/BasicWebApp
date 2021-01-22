6bbb9cb125dd02d2ba2ea1a36d401a5122d5ba46
Patrick Henderson <pah17@ic.ac.uk>
2021-01-22 13:05:29 +0000

    Importing assertThat

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class QueryProcessorTest {
 
